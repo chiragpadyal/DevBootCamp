99a6371edc2493f0476681fa6aa724992818219f
Chirag Padyal <FreakStar03>
2022-07-22 21:34:15 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from "react";
+// import "./index.css";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+import Homepage from "./Components/Homepage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Navbar />
+        <Homepage />
+        <Footer />
+      </>
+    );
+  }
 }
-
 export default App;
